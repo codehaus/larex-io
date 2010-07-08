fe8fa0cfe8fc2392256017a36cda9838a3062c77
Simone Bordet <simone.bordet@gmail.com>
2010-07-08 13:11:34 +0200

    Cosmetics.

--- src/test/java/org/codehaus/larex/io/connector/ssl/SSLClientConnectorTest.java
+++ src/test/java/org/codehaus/larex/io/connector/ssl/SSLClientConnectorTest.java
@@ -46,9 +46,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-/**
- *
- */
 public class SSLClientConnectorTest extends AbstractTestCase
 {
     private SSLClientConnector clientConnector;
