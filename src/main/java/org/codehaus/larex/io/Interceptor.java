35893174345c996b7eb1ffc105d4f1e11a5ae064
Simone Bordet <simone.bordet@gmail.com>
2011-03-02 23:16:08 +0100

    Javadocs.

--- src/main/java/org/codehaus/larex/io/Interceptor.java
+++ src/main/java/org/codehaus/larex/io/Interceptor.java
@@ -18,40 +18,111 @@ package org.codehaus.larex.io;
 
 import java.nio.ByteBuffer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+/**
+ * <p>Intercepts read, write and close events to allow transformation
+ * of incoming/outgoing data.</p>
+ * <p>Incoming data flows into the interceptor before arriving to the {@link Connection}.</p>
+ * <p>Outgoing data flows from the connection to the interceptor to the {@link Controller}.</p>
+ * <p>{@link Interceptor}s are added to a controller via
+ * {@link Controller#addInterceptor(Interceptor)} and form a chain that is processed
+ * in the same order they have been added to the controller.</p>
+ */
 public interface Interceptor
 {
-    Interceptor getNext();
+    /**
+     * @return the next interceptor in the chain
+     */
+    public Interceptor getNext();
 
-    void setNext(Interceptor interceptor);
+    /**
+     * @param interceptor the next interceptor in the chain
+     */
+    public void setNext(Interceptor interceptor);
 
-    void onOpen();
+    /**
+     * <p>Callback method invoked when the connection is opened,
+     * before {@link Connection#openEvent()}.</p>
+     */
+    public void onOpen();
 
-    void onReadTimeout();
+    /**
+     * <p>Callback method invoked when the connection has read bytes,
+     * before {@link Connection#readEvent(ByteBuffer)}.</p>
+     *
+     * @param buffer the buffer with the bytes read
+     */
+    public void onRead(ByteBuffer buffer);
 
-    void onRead(ByteBuffer buffer);
+    /**
+     * <p>Callback method invoked when the read is completed for the
+     * time being, before {@link Connection#readEndEvent()}.</p>
+     *
+     * @return whether to set read interest to receive further read events
+     */
+    public boolean onReadEnd();
 
-    boolean onReadEnd();
+    /**
+     * <p>Callback method invoked when the read timed out, before
+     * {@link Connection#readTimeoutEvent()}.</p>
+     */
+    public void onReadTimeout();
 
-    void onWrite();
+    /**
+     * <p>Callback method invoked when the controller writes data,
+     * after {@link Controller#write(ByteBuffer)}.</p>
+     *
+     * @param buffer the buffer to write
+     * @return the number of bytes written
+     */
+    public int write(ByteBuffer buffer);
 
-    void onWriteTimeout();
+    /**
+     * <p>Callback method invoked when the underlying connection is
+     * ready to be written, before {@link Connection#writeEvent()}.</p>
+     */
+    public void onWrite();
 
-    int write(ByteBuffer buffer);
+    /**
+     * <p>Callback method invoked when the write timed out, before
+     * {@link Connection#writeTimeoutEvent()}.</p>
+     */
+    public void onWriteTimeout();
 
-    void onRemoteClose();
+    /**
+     * <p>Callback method invoked when the remote peer has closed its
+     * side of the underlying connection, before {@link Connection#remoteCloseEvent()}.</p>
+     */
+    public void onRemoteClose();
 
-    void onClosing(StreamType type);
+    /**
+     * <p>Callback method invoked just before the controller is closing
+     * the underlying connection's stream type, before {@link Connection#closingEvent(StreamType)}.</p>
+     *
+     * @param type the stream type that is being closed
+     */
+    public void onClosing(StreamType type);
 
-    void onClosed(StreamType type);
+    /**
+     * <p>Callback method invoked when the controller is closing the underlying
+     * connection's stream type, before {@link Controller#close(StreamType)}.</p>
+     *
+     * @param type the stream type to close
+     */
+    public void close(StreamType type);
 
-    void close(StreamType type);
+    /**
+     * <p>Callback method invoked just after the controller has closed its
+     * side of the underlying connection, before {@link Connection#closedEvent(StreamType)}.</p>
+     *
+     * @param type the stream type that was closed
+     */
+    public void onClosed(StreamType type);
 
+    /**
+     * <p>Implementation of {@link Interceptor} that forwards the events to the next interceptor in the chain.</p>
+     */
     public static class Forwarder implements Interceptor
     {
-        protected final Logger logger = LoggerFactory.getLogger(getClass());
         private volatile Interceptor next;
 
         @Override
