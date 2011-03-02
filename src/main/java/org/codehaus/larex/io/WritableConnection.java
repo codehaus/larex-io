a727133c3052e5f0a550328a712139c2c8d170c6
Simone Bordet <simone.bordet@gmail.com>
2011-03-02 13:21:25 +0100

    Javadocs.

--- src/main/java/org/codehaus/larex/io/WritableConnection.java
+++ src/main/java/org/codehaus/larex/io/WritableConnection.java
@@ -2,6 +2,13 @@ package org.codehaus.larex.io;
 
 import java.nio.ByteBuffer;
 
+/**
+ * <p>Partial implementation of {@link Connection} that provides buffering writes
+ * functionalities and inherits close functionalities.</p>
+ * <p>Writes are buffered so that the buffer to be written is always fully consumed
+ * but in the event that it cannot be fully written to the underlying connection
+ * the remaining bytes are buffered and written on first occasion.</p>
+ */
 public abstract class WritableConnection extends ClosableConnection
 {
     private final NonBlockingWriter writer;
@@ -34,6 +41,16 @@ public abstract class WritableConnection extends ClosableConnection
             writer.closingEvent();
     }
 
+    /**
+     * <p>Writes the bytes in the given buffer with non-blocking semantic.</p>
+     * <p>The given buffer will be fully consumed, either because it has been
+     * fully written or because the remaining bytes have been copied into a
+     * temporary buffer to be written later; the buffer is therefore disposable
+     * upon return from this method.</p>
+     *
+     * @param buffer the buffer to write
+     * @return the number of bytes of the given buffer that have been written
+     */
     public final int write(ByteBuffer buffer)
     {
         return writer.write(buffer);
